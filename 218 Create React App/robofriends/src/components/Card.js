33ae05571260d01c5cb67225b91a48e4eaf6d23b
Emerald-Coat <rikzafatiyamulya.2019@student.uny.ac.id>
2022-09-23 01:13:17 +0700

    Reach : Finished Robofriend

--- 218 Create React App/robofriends/src/components/Card.js
+++ 218 Create React App/robofriends/src/components/Card.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const Card = ({name, email, id}) => {
     return (
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw3 shadow-5">
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw3 shadow-5"
+        style={{
+            width : 'auto',
+            maxWidth : '230px',
+            // height : 'auto',
+            height: '350px',
+            overflow: 'hidden'
+        }}>
             <img alt="robot" src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
